5e3b7a15bac6f1a8d35a99494e8e55a04af57fb1
Gatewayfroze <panotsodsri@gmail.com>
2017-04-09 23:03:24 +0700

    test music

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,7 +6,11 @@ import javafx.scene.ImageCursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Main extends Application {
     private Stage stage;
@@ -25,6 +29,12 @@ public class Main extends Application {
         scene.setCursor(new ImageCursor(cursor,
                 cursor.getWidth() / 2,
                 cursor.getHeight() /2));
+        String musicFile = "src/bgm.mp3";     // For example
+
+        Media sound = new Media(new File(musicFile).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(sound);
+        mediaPlayer.play();
+        mediaPlayer.setVolume(1);
 
         return scene;
     }
